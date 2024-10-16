8767a1ad886fd218b4a06f7b708f2b5dd53c4b04
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-10-16 19:05:44 +0430

    Fetch the data in the create new component

--- app/snippets/new/page.tsx
+++ app/snippets/new/page.tsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import { db } from '@/db';
+import { redirect } from 'next/navigation';
 
 export default function NewSnippetPage() {
+  async function createSnippet(formData: FormData) {
+    // This needs to be a server action
+    'use server';
+
+    // Checks the user's input
+    const title = formData.get('title') as string;
+    const code = formData.get('code') as string;
+
+    // Create a new recored in the database
+    const snippet = await db.snippet.create({ data: { code, title } });
+    console.log(snippet);
+    // Redirect the user to the home page
+    redirect('/');
+  }
+
   return (
-    <form>
+    <form action={createSnippet}>
       <h3 className="font-bold m-3 text-lg">Create a Snippet</h3>
       <div className="flex flex-col gap-4">
+        {/* Title */}
         <div className="flex gap-4 items-start">
           <label htmlFor="title" className="w-12">
             Title
@@ -17,16 +34,18 @@ export default function NewSnippetPage() {
           />
         </div>
 
+        {/* Code */}
         <div className="flex gap-4 items-start">
           <label htmlFor="code" className="w-12">
             Code
           </label>
           <textarea
-            name="title"
-            id="title"
+            name="code"
+            id="code"
             className="w-full rounded border p-2 h-40"
           ></textarea>
         </div>
+        {/* Submit */}
         <div className="flex justify-end">
           <button
             type="submit"
