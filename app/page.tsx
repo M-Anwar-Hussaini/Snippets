353c9f314bc9798380e3f410d30c9edb070c587e
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-10-16 19:04:55 +0430

    List all snippets on the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
-export default function Home() {
-  return <div>Home Page</div>;
+import { db } from '@/db';
+
+export default async function Home() {
+  const snippet = await db.snippet.findMany();
+  const renderedSnippets = snippet.map((snippet) => (
+    <div key={snippet.id}>{snippet?.title}</div>
+  ));
+  return <div>{renderedSnippets}</div>;
 }
