d7d7ba4242b9f3257bbcfb6f52b0fcbda59686d8
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-10-16 17:24:19 +0430

    Change the layout of the layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div className="max-w-2xl mx-auto p-12">{children}</div>
+      </body>
     </html>
   );
 }
