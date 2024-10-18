a676c101455e1287a871908cb7105c7a905b7b8c
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-10-18 08:27:11 +0430

    Add the not-found page in the snippet

---
+++ app/snippets/[id]/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <div>
+      <h1 className="text-xl font-bold">
+        Sorry but we couldn&apos;t find this page
+      </h1>
+    </div>
+  );
+}
