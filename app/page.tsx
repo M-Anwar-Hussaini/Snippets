abf06b8a7c884bd58311c41870430a5fd0df6da2
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-10-18 08:28:43 +0430

    Add the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,24 @@
 import { db } from '@/db';
+import Link from 'next/link';
 
 export default async function Home() {
   const snippet = await db.snippet.findMany();
   const renderedSnippets = snippet.map((snippet) => (
-    <div key={snippet.id}>{snippet?.title}</div>
+    <Link
+      className="flex items-center justify-between border p-2 my-2"
+      href={`/snippets/${snippet.id}`}
+      key={snippet.id}
+    >
+      <div>{snippet?.title}</div>
+      <div>View</div>
+    </Link>
   ));
-  return <div>{renderedSnippets}</div>;
+  return (
+    <div className="flex flex-col">
+      <div className="flex space-x-3">
+        <Link href={'/snippets/new'}>New Snippet</Link>
+      </div>
+      <div>{renderedSnippets}</div>
+    </div>
+  );
 }
