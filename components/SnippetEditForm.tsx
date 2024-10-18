9e99e17ea6917c4c6883506c12fa6cc66b6f0f87
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-10-18 08:53:39 +0430

    Add editor to the Snippet form

--- components/SnippetEditForm.tsx
+++ components/SnippetEditForm.tsx
@@ -1,11 +1,27 @@
 'use client';
-
 import { Snippet } from '@prisma/client';
+import Editor from '@monaco-editor/react';
+import { useState } from 'react';
 
 interface SnippetEditFormProps {
   snippet: Snippet;
 }
 
 export default function SnippetEditForm({ snippet }: SnippetEditFormProps) {
-  return <div>Client componet with snippet title: {snippet.title}</div>;
+  const [code, setCode] = useState(snippet.code);
+  const handleEditorChange = (value: string = '') => {
+    setCode(value);
+  };
+  return (
+    <div>
+      <Editor
+        height="40vh"
+        theme="vs-dark"
+        language="javascript"
+        defaultValue={code}
+        options={{ minimap: { enabled: false } }}
+        onChange={handleEditorChange}
+      />
+    </div>
+  );
 }
