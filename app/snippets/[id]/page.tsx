83c7b2ffeb716a6e26a69a6c8b07c7c943e6b24b
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-10-18 08:28:30 +0430

    Show the details of a snippet

--- app/snippets/[id]/page.tsx
+++ app/snippets/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { notFound } from 'next/navigation';
 import { db } from '@/db';
+import Link from 'next/link';
 
 interface SnippetShowPageProps {
   params: {
@@ -9,10 +10,30 @@ interface SnippetShowPageProps {
 
 export default async function SnippetShowPage(props: SnippetShowPageProps) {
   const snippet = await db.snippet.findFirst({
-    where: { id: Number(props.params.id) },
+    where: { id: parseInt(props.params.id) },
   });
   if (!snippet) {
     return notFound();
   }
-  return <div>{snippet?.title}</div>;
+  return (
+    <div>
+      <div className="flex m-4 items-center justify-between">
+        <h1 className="text-xl font-bold">{snippet?.title}</h1>
+        <div className="flex space-x-4">
+          <Link
+            className="px-3 py-1 hover:bg-blue-400 transition duration-300 rounded border"
+            href={`/snippets/${props.params.id}/edit`}
+          >
+            Edit
+          </Link>
+          <button className="px-3 py-1 hover:bg-red-600 transition duration-300 rounded border">
+            Delete
+          </button>
+        </div>
+      </div>
+      <pre className="p-3 border rounded bg-gray-200 border-gray-400">
+        <code>{snippet.code}</code>
+      </pre>
+    </div>
+  );
 }
